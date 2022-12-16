7e93be5bb3ddccbc76d8bf48dcc62dbee2492e6e
Galang S <Galang S@DESKTOP-D94OJ1M>
2022-12-16 10:15:03 +0700

    Add Test Suite with Data Files Excel

---
+++ Test Suites/Regression 16122022/Regresi All Login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regresi All Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5e66dbc3-8eb9-4ad0-897c-d599e22149d9</testSuiteGuid>
+   <testCaseLink>
+      <guid>cd5430d7-9408-44d3-af71-507fcb16a804</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login with Data</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>49748721-4078-4ef4-883e-ebe722d87a66</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>49748721-4078-4ef4-883e-ebe722d87a66</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>User</value>
+         <variableId>58d31edb-58a8-49b9-b730-48817748fca6</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>49748721-4078-4ef4-883e-ebe722d87a66</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Pass</value>
+         <variableId>be9437af-2153-4c25-bbc9-8c75f85a2cfe</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
